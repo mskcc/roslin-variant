fadb69e63401cd340cbae796bd6375d436b721a9
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-03-23 14:58:56 -0400

    Handle the case when no jobs are found

--- setup/data/inputs/show-job-status.sh
+++ setup/data/inputs/show-job-status.sh
@@ -12,6 +12,12 @@ fi
 # get LSF job ids
 JOB_IDS=`grep -o -P "Got the job id: \d+" ${STDOUT_LOG} | awk -F':' '{ print $2 }' | uniq`
 
+if [ -z $JOB_IDS ]
+then
+  echo "No jobs found"
+  exit 1
+fi
+
 # get job id, name, final status, resources requeted and/or used
 # https://www.ibm.com/support/knowledgecenter/en/SSETD4_9.1.2/lsf_command_ref/bjobs.1.html
 csv=`bjobs -o 'jobid stat job_name max_mem avg_mem memlimit max_req_proc delimiter=","' ${JOB_IDS}`
