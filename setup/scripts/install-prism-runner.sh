3e9626ed1dcb993b06e7e0f6724d126b46b250c9
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-03-27 13:29:19 -0400

    Copy job archive and status script

--- setup/scripts/install-prism-runner.sh
+++ setup/scripts/install-prism-runner.sh
@@ -7,6 +7,9 @@ source ./settings.sh
 
 # fixme: use symlink
 cp ../bin/prism-runner/prism-runner-${VERSION}.sh ${PRISM_BIN_PATH}/bin/prism-runner/prism-runner.sh
+cp ../bin/prism-runner/prism-job-archive-${VERSION}.sh ${PRISM_BIN_PATH}/bin/prism-runner/prism-job-archive.sh
+cp ../bin/prism-runner/prism-job-status-${VERSION}.sh ${PRISM_BIN_PATH}/bin/prism-runner/prism-job-status.sh
+cp ../bin/prism-runner/tree.py ${PRISM_BIN_PATH}/bin/prism-runner/tree.py
 
 if [ "$PRISM_SYSTEM_WIDE_INSTALL" == "YES" ]
 then
