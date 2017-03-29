4859e50e3294aaee35b5255ce4ca6764977feb31
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-03-28 22:48:36 -0400

    Refactor, update

--- setup/scripts/install-prism-runner.sh
+++ setup/scripts/install-prism-runner.sh
@@ -5,10 +5,10 @@ VERSION='1.0.0'
 # load config
 source ./settings.sh
 
-# fixme: use symlink
-cp ../bin/prism-runner/prism-runner-${VERSION}.sh ${PRISM_BIN_PATH}/bin/prism-runner/prism-runner.sh
-cp ../bin/prism-runner/prism-job-archive-${VERSION}.sh ${PRISM_BIN_PATH}/bin/prism-runner/prism-job-archive.sh
-cp ../bin/prism-runner/prism-job-status-${VERSION}.sh ${PRISM_BIN_PATH}/bin/prism-runner/prism-job-status.sh
+# copy scripts
+cp ../bin/prism-runner/prism-runner.sh ${PRISM_BIN_PATH}/bin/prism-runner/prism-runner.sh
+cp ../bin/prism-runner/prism-job-archive.sh ${PRISM_BIN_PATH}/bin/prism-runner/prism-job-archive.sh
+cp ../bin/prism-runner/prism-job-status.sh ${PRISM_BIN_PATH}/bin/prism-runner/prism-job-status.sh
 cp ../bin/prism-runner/tree.py ${PRISM_BIN_PATH}/bin/prism-runner/tree.py
 
 if [ "$PRISM_SYSTEM_WIDE_INSTALL" == "YES" ]
