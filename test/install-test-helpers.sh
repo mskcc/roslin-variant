545aa1d69781338800b7b98d41ab10c95d9fbc12
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-04-01 09:19:26 -0400

    Add comment

--- test/install-test-helpers.sh
+++ test/install-test-helpers.sh
@@ -5,6 +5,7 @@ git clone https://github.com/sstephenson/bats.git ./helpers/bats
 cd ./helpers/bats
 sudo ./install.sh /usr/local
 
+# install bats helper libraries
 git clone https://github.com/ztombol/bats-support.git ./helpers/bats-support
 git clone https://github.com/ztombol/bats-assert.git ./helpers/bats-assert
 git clone https://github.com/ztombol/bats-file.git ./helpers/bats-file
