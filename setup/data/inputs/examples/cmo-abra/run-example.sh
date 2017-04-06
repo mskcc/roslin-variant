4f8ded917a60527d81b461d513b74efa5de94a42
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-04-06 14:55:52 -0400

    Use collision-free temp directory for ABRA

--- setup/data/inputs/examples/cmo-abra/run-example.sh
+++ setup/data/inputs/examples/cmo-abra/run-example.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# modify inputs.yaml to have an user-specific, collision safe working directory
+uuid=`python -c 'import uuid; print str(uuid.uuid1())'`
+tmpdir="$HOME/tmp/$uuid"
+eval "echo \"$(cat inputs.yaml.template)\"" > inputs.yaml
+
 prism-runner.sh \
     -w cmo-abra/0.92/cmo-abra.cwl \
     -i inputs.yaml \
