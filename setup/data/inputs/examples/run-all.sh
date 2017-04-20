ff9aafd5502e65592aaef1c548c5ed395267a2c4
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-04-20 11:35:15 -0400

    Add output testing code

---
+++ setup/data/inputs/examples/run-all.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# e.g. list='cmo-abra cmo-bwa-mem'
+list=`find . -maxdepth 1 -type d -not -path . -exec bash -c "echo {} | cut -c3-" \;`
+
+for dir in $list
+do
+
+  # skip the data directory
+  if [ "$dir" == "data" ]; then continue; fi
+
+  # skip bsub-of-prism-runner
+  # we will treat this separately at the end
+  if [ "$dir" == "bsub-of-prism-runner" ]; then continue; fi
+
+  # skip module-2 for now because at this moment it uses singleMachine, not lsf
+  # so we will treat this separately at the end
+  if [ "$dir" == "module-2" ]; then continue; fi 
+
+  echo $dir
+
+  cd $dir
+
+  if [ -e "./run-example.sh" ]
+  then
+    ./run-example.sh | tee ../results.$dir.txt
+  fi
+
+  cd ..
+
+done
+
+
+# bsub-of-prism-runner
+bsub -q test -K -cwd ./bsub-of-prism-runner \
+  -eo ../results.bsub-of-prism-runner.stderr.txt \
+  -oo ../results.bsub-of-prism-runner.txt \
+  "./run-example.sh"
+
+# module-2
+bsub -q test -K -cwd ./module-2 \
+  -eo ../results.module-2.stderr.txt \
+  -oo ../results.module-2.txt \
+  "./run-example.sh"
