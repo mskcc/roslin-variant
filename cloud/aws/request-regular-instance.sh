9c5e57c596b59ebc15ba8561bea8b2f609901eb5
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-05-03 16:42:47 -0400

    Fix

--- cloud/aws/request-regular-instance.sh
+++ cloud/aws/request-regular-instance.sh
@@ -13,6 +13,9 @@ aws ec2 run-instances \
   --cli-input-json file://specification.t2micro.json \
   --user-data file://install.sh | tee ${result_json}
 
+# aws ec2 run-instances \
+#   --cli-input-json file://specification.t2micro.json | tee ${result_json}
+
 cat ${result_json}
 
 # get instance id
@@ -20,18 +23,37 @@ instance_id=`jq -r .Instances[].InstanceId ${result_json}`
 
 echo ${instance_id}
 
+# add tags
+aws ec2 create-tags \
+    --resources ${instance_id} \
+    --tags Key=Name,Value=prism-pipeline-test Key=Owner,Value=chunj
+
 # wait till instance is in running state
 aws ec2 wait instance-running --instance-ids ${instance_id}
 
 # get public dns name
-public_dns_name=`aws ec2 describe-instances --instance-ids ${instance_id} | jq ".Reservations[0].Instances[0].PublicDnsName"`
+public_dns_name=`aws ec2 describe-instances --instance-ids ${instance_id} | jq -r ".Reservations[0].Instances[0].PublicDnsName"`
 
 echo ${public_dns_name}
 
-sleep 30
+sleep 10
 
+# check installed software
 ssh -i ~/mskcc-chunj.pem -o "StrictHostKeyChecking no" ubuntu@${public_dns_name} "cat /var/log/prism-software-versions.txt"
-# ssh -i "~/mskcc-chunj.pem" ${public_dns_name} "tail /var/log/cloud-init-output.log"
+
+# poll every 5 sec if cloud-init is finished
+while true
+do
+  ssh -i ~/mskcc-chunj.pem -o "StrictHostKeyChecking no" ubuntu@${public_dns_name} "test -f /var/lib/cloud/instances/${instance_id}/boot-finished"
+  if [ $? -eq 0 ]
+  then
+    break
+  fi
+  echo "Waiting..."
+  sleep 10
+done
 
 # delete temporary
 rm -rf ${result_json}
+
+echo "Done."
