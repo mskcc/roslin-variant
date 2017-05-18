f594b233d3271330e0fd3f85d96939c5a97f3941
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-05-18 16:41:04 -0400

    move checksum check to the bottom otherwise bash -e causes premature termination some cases

--- setup/scripts/install-cwl-wrappers.sh
+++ setup/scripts/install-cwl-wrappers.sh
@@ -9,10 +9,6 @@ cp -R ../cwl-wrappers/* ${PRISM_BIN_PATH}/pipeline/
 # copy RDF schemas that are referenced by cwl wrappers
 cp -R ../schemas/* ${PRISM_BIN_PATH}/schemas/
 
-# check md5 checksum
-cd ${PRISM_BIN_PATH}/pipeline/${PRISM_VERSION}
-md5sum -c checksum.dat
-
 # use pre-fetched local schemas instead of going over the Internet to fetch
 root_dir="${PRISM_BIN_PATH}/pipeline/"
 for file in `find ${root_dir} -name "*.cwl"`
@@ -49,3 +45,6 @@ do
 
 done
 
+# check md5 checksum
+cd ${PRISM_BIN_PATH}/pipeline/${PRISM_VERSION}
+md5sum -c checksum.dat
