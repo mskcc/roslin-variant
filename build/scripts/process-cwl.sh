916ffd3d826faa74ac923168dda7778535fecf0c
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-05-18 09:18:15 -0400

    Skip toolname that starts with @

--- build/scripts/process-cwl.sh
+++ build/scripts/process-cwl.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
 usage()
 {
@@ -23,7 +23,7 @@ do
     case $OPTION in
         t) TOOL_NAME=$OPTARG ;;
         v) TOOL_VERSION=$OPTARG ;;
-        c) CMO_WRAPPER=$OPTARG ;;        
+        c) CMO_WRAPPER=$OPTARG ;;
         h) usage; exit 1 ;;
         *) usage; exit 1 ;;
     esac
@@ -55,8 +55,8 @@ then
 else
 
     # don't do this block if tool name starts with @
-    # because the cwl wrapper that we are building doesn't depend on any containers 
-    if [ ! "${TOOL_NAME}" == @* ]
+    # because the cwl wrapper that we are building doesn't depend on any containers
+    if [ ! "${TOOL_NAME:0:1}" == "@" ]
     then
         # do not use "-t" because docker messes up stdout and stderr
         # hack: an extra space at the end helps cases where a container has multiple entry points
@@ -89,7 +89,7 @@ sed -i "s/type: str$/type: string/g" ${TOOL_DIRECTORY}/${CMO_WRAPPER_WITH_DASH}.
 # remove unnecessasry u (unicode)
 sed -i "s/u'/'/g" ${TOOL_DIRECTORY}/${CMO_WRAPPER_WITH_DASH}.cwl
 
-# postprocess: add metadata, requirements section, other necessary 
+# postprocess: add metadata, requirements section, other necessary
 python ./postprocess_cwl.py \
     -f ${TOOL_DIRECTORY}/${CMO_WRAPPER_WITH_DASH}.cwl \
     -v ${TOOL_VERSION} \
@@ -104,30 +104,3 @@ then
 fi
 
 tree ${TOOL_DIRECTORY}
-
-#fixme: is it the right location to do this? or should we do right after create containers?
-# modify prism_resources.json so that cmo in production can call sing.sh (singularity wrapper)
-case ${TOOL_NAME} in
-    pindel)
-        # pindel needs special treament since pindel container has two executables "pindel" and "pindel2vcf"
-        python ./update_resource_def.py -f ../cwl-wrappers/prism_resources.json pindel ${TOOL_VERSION} "sing.sh ${TOOL_NAME} ${TOOL_VERSION} pindel"
-        python ./update_resource_def.py -f ../cwl-wrappers/prism_resources.json pindel2vcf ${TOOL_VERSION} "sing.sh ${TOOL_NAME} ${TOOL_VERSION} pindel2vcf"
-        ;;
-    vardict)
-        # vardict needs special treament since vardict container has one R script and one Perl script to be exposed
-        python ./update_resource_def.py -f ../cwl-wrappers/prism_resources.json vardict ${TOOL_VERSION} "sing.sh ${TOOL_NAME} ${TOOL_VERSION} vardict"
-
-        # an extra space needed at the end because cmo will append either "testsomatic.R" or "var2vcf_paired.pl"
-        # and we need to make sure it's treated as an argument.
-        # e.g. sing.sh vardict 1.4.6 testsomatic.R
-        python ./update_resource_def.py -f ../cwl-wrappers/prism_resources.json vardict_bin ${TOOL_VERSION} "sing.sh ${TOOL_NAME} ${TOOL_VERSION} "
-        ;;
-    vcf2maf)
-        # an extra space needed at the end because cmo will append "vcf2maf.pl"
-        # e.g. sing.sh vcf2maf 1.6.12 vcf2maf.pl
-        python ./update_resource_def.py -f ../cwl-wrappers/prism_resources.json vcf2maf ${TOOL_VERSION} "sing.sh ${TOOL_NAME} ${TOOL_VERSION} "
-        ;;
-    *)
-        python ./update_resource_def.py -f ../cwl-wrappers/prism_resources.json ${TOOL_NAME} ${TOOL_VERSION} "sing.sh ${TOOL_NAME} ${TOOL_VERSION}"
-        ;;
-esac
