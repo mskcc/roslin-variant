b29a67285029d85d88ea0ed00fa3de1ca7640dad
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-05-23 15:37:48 -0400

    Fix abra temp directory issue

--- setup/data/inputs/examples/module-2/run-example.sh
+++ setup/data/inputs/examples/module-2/run-example.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# modify inputs.yaml.template to have an user-specific, collision safe working directory
+# modify inputs.yaml.template to have an user-specific,
+# collision safe working directory for abra
 uuid=`python -c 'import uuid; print str(uuid.uuid1())'`
-tmpdir="$HOME/tmp/prism-$uuid"
-mkdir -p "$HOME/tmp"
+# $tmpdir is the dynamic variable used in the template
+# create right under /scratch, otherwise it will fail
+tmpdir="/scratch/prism-abra-$uuid"
+# populate the template
 eval "echo \"$(cat inputs.yaml.template)\"" > inputs.yaml
 
 prism-runner.sh \
