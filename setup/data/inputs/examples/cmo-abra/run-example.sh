0bf53b7b53e172ef598997f0c14a2075474076a8
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-05-24 19:54:47 -0400

    Reorg

--- setup/data/inputs/examples/cmo-abra/run-example.sh
+++ setup/data/inputs/examples/cmo-abra/run-example.sh
@@ -3,9 +3,11 @@
 # modify inputs.yaml.template to have an user-specific,
 # collision safe working directory for abra
 uuid=`python -c 'import uuid; print str(uuid.uuid1())'`
+
 # $tmpdir is the dynamic variable used in the template
 # create right under /scratch, otherwise it will fail
 tmpdir="/scratch/prism-abra-$uuid"
+
 # populate the template
 eval "echo \"$(cat inputs.yaml.template)\"" > inputs.yaml
 
