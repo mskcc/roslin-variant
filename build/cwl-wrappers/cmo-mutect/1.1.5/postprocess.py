3d9046a996cf84486253738f958fcde9b6fc855f
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-05-25 06:50:55 -0400

    Add MuTect 1.1.5

---
+++ build/cwl-wrappers/cmo-mutect/1.1.5/postprocess.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+"""postprocess"""
+
+import argparse
+import ruamel.yaml
+
+
+def read(filename):
+    """return file contents"""
+
+    with open(filename, 'r') as file_in:
+        return file_in.read()
+
+
+def write(filename, cwl):
+    """write to file"""
+
+    with open(filename, 'w') as file_out:
+        file_out.write(cwl)
+
+
+def main():
+    """main function"""
+
+    parser = argparse.ArgumentParser(description='postprocess')
+
+    parser.add_argument(
+        '-f',
+        action="store",
+        dest="filename_cwl",
+        help='Name of the cwl file',
+        required=True
+    )
+
+    params = parser.parse_args()
+
+    cwl = ruamel.yaml.load(read(params.filename_cwl),
+                           ruamel.yaml.RoundTripLoader)
+
+    # this CWL is genereated specifically for cmo_mutect 1.1.5
+    cwl['baseCommand'] = ['cmo_mutect', '--version', '1.1.5']
+    cwl['inputs']['out']['type'] = ['null', 'string', 'File']
+    del cwl['inputs']['version']
+    del cwl['inputs']['java_version']
+
+    #-->
+    # fixme: until we can auto generate cwl for MuTect
+    # set outputs using outputs.yaml
+    import os
+    cwl['outputs'] = ruamel.yaml.load(
+        read(os.path.dirname(params.filename_cwl) + "/outputs.yaml"),
+        ruamel.yaml.RoundTripLoader)['outputs']
+    #<--
+
+    write(params.filename_cwl, ruamel.yaml.dump(
+        cwl, Dumper=ruamel.yaml.RoundTripDumper))
+
+
+if __name__ == "__main__":
+
+    main()
