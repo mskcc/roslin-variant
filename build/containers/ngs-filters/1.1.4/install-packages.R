d06f16f57341322c51b048c0bad8a2402df9d669
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-06-09 09:23:06 -0400

    Remove wes-filters 1.1.3, add ngs-filters 1.1.4-beta

--- build/containers/ngs-filters/1.1.4/install-packages.R
+++ build/containers/ngs-filters/1.1.4/install-packages.R
@@ -1,5 +1,5 @@
 #!/usr/bin/Rscript --vanilla
 
-pkgs = c("stringr", "data.table", "argparse", "reshape2", "dplyr", "tidyr")
+pkgs = c("stringr", "data.table", "argparse", "reshape2", "dplyr", "tidyr", "kimisc")
 
 install.packages(pkgs, repos='http://cran.us.r-project.org')
