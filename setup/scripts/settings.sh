475eaa04501ed0b8333716c1e6f661bf0e9cf567
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-06-19 23:45:29 -0400

    Minor update

--- setup/scripts/settings.sh
+++ setup/scripts/settings.sh
@@ -1,6 +1,5 @@
 export PRISM_VERSION="1.0.0"
 
-# fixme: use / in the production
 PRISM_ROOT="/ifs/work/chunj/prism-proto"
 
 #--> the following paths will be supplied to singularity as bind points
@@ -14,10 +13,10 @@ export PRISM_DATA_PATH="${PRISM_ROOT}/ifs"
 # other paths that we'd like to bind (space separated)
 export PRISM_EXTRA_BIND_PATH="/scratch"
 
-#<--
-
-# input files to pipeline (e.g. fastq files)
+# workspace
 export PRISM_INPUT_PATH="${PRISM_ROOT}/ifs/prism/inputs"
+
+#<--
 
 # path to singularity executable
 # singularity is expected to be found at the same location regardless of the nodes you're on
--- setup/scripts/settings.template.sh
+++ setup/scripts/settings.template.sh
@@ -1,6 +1,5 @@
 export PRISM_VERSION="{{ version }}"
 
-# fixme: use / in the production
 PRISM_ROOT="{{ root }}"
 
 #--> the following paths will be supplied to singularity as bind points
@@ -14,10 +13,10 @@ export PRISM_DATA_PATH="${PRISM_ROOT}/{{ binding_data }}"
 # other paths that we'd like to bind (space separated)
 export PRISM_EXTRA_BIND_PATH="{{ binding_extra }}"
 
-#<--
-
-# input files to pipeline (e.g. fastq files)
+# workspace
 export PRISM_INPUT_PATH="${PRISM_ROOT}/{{ binding_workspace }}"
+
+#<--
 
 # path to singularity executable
 # singularity is expected to be found at the same location regardless of the nodes you're on
