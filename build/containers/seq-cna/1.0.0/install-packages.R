86d966342ed6bfa921219d84a1af1720a2192c16
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-07-26 16:28:55 -0400

    Update seqCNA (WIP)

--- build/containers/seq-cna/1.0.0/install-packages.R
+++ build/containers/seq-cna/1.0.0/install-packages.R
@@ -1,7 +1,16 @@
 #!/usr/bin/Rscript
 
+# install required R libraries
 pkgs = c("devtools", "stringr", "data.table", "xlsx", "RJSONIO", "Rcpp")
 
 install.packages(pkgs, repos='http://cran.us.r-project.org')
 
-# devtools::install_github("mskcc/pctGCdata")
+
+# install R datasets of GC percentages for human and mouse genomes
+devtools::install_github("mskcc/pctGCdata")
+
+
+# install necessary BioConductor packages
+source('https://bioconductor.org/biocLite.R')
+
+biocLite(c('IRanges', 'DNAcopy', 'Rsamtools'))
