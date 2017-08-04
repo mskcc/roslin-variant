5f58c827725bd55aa775fb953491bff347efe176
Nikhil <nikhilkumar516@gmail.com>
2017-08-04 11:19:56 -0400

    Added roslin-qc container

---
+++ build/containers/roslin-qc/0.5.0/install-packages.R
@@ -0,0 +1,7 @@
+#!/usr/bin/Rscript
+
+# install required R libraries
+pkgs = c("ggplot2","gplots","corrplot","scales","reshape","plyr","RColorBrewer")
+
+install.packages(pkgs, repos='http://cran.us.r-project.org')
+
