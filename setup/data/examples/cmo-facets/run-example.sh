6e0f1e7dd094d115a4f1a02d91e6efb601471a5b
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-08-15 12:29:37 -0400

    Minor update

--- setup/data/examples/cmo-facets/run-example.sh
+++ setup/data/examples/cmo-facets/run-example.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 prism-runner.sh \
-    -w cmo-facets.doFacets/1.5.4/cmo-facets.doFacets.cwl \
+    -w cmo-facets.doFacets/1.5.5/cmo-facets.doFacets.cwl \
     -i inputs.yaml \
-    -b singleMachine -d
+    -b lsf
