4ffaaccedab8763cc1ffd966b54467d55d68f755
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-08-21 14:08:20 -0400

    Restructure

---
+++ setup/data/examples/Proj_DMPVAL_01/batch/batch16/batch-11/run-example.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# nohup prism-runner.sh \
+# 	-w project-workflow.cwl \
+# 	-i inputs.yaml \
+# 	-b lsf &
+
+prism_submit.py \
+   --id Proj_DMPVAL_01_11 \
+   --path . \
+   --workflow project-workflow.cwl
