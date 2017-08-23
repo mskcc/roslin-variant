e4837793535ac40eb3a32ef94777efb1e1d40c17
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-08-23 13:20:29 -0400

    Remove unnecessary

--- setup/bin/prism-runner/prism-cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# remove tmp under prism bin
-rm -rf ${PRISM_BIN_PATH}/tmp/*
-# find ${PRISM_BIN_PATH}/tmp -maxdepth 1 -type d -name ! . -user chunj | xargs -I {} rm -rf {}
-
-# remove tmp under the current
-find ${PRISM_INPUT_PATH}/chunj -name tmp* | xargs -I {} rm -rf {}
