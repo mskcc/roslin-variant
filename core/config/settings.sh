247cf523dccd285276a829db14b0f032552ffe75
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-09-22 16:26:04 +0300

    Roslin Core + prism to roslin

--- core/config/settings.sh
+++ core/config/settings.sh
@@ -5,9 +5,6 @@ export ROSLIN_CORE_VERSION="1.0.0"
 # path to all the Roslin Core versions are/will be installed
 export ROSLIN_CORE_ROOT="/ifs/work/pi/roslin-core"
 
-# path to all the Roslin Pipelines are/will be installed
-export ROSLIN_PIPELINE_INSTALL_PATH="/ifs/work/pi/roslin-pipelines"
-
 # path for a specific version of Roslin Core
 export ROSLIN_CORE_PATH="${ROSLIN_CORE_ROOT}/${ROSLIN_CORE_VERSION}"
 
