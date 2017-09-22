6a0e45cc977653718f73483bd326645602a6edaf
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-09-22 09:59:09 +0300

    Roslin Core + prism to roslin

--- core/bin/install/install-core.sh
+++ core/bin/install/install-core.sh
@@ -14,17 +14,4 @@ cp -r ../../config/* ${ROSLIN_CORE_CONFIG_PATH}
 cp -r ../../schemas/* ${ROSLIN_CORE_SCHEMA_PATH}
 cp ${ROSLIN_CORE_BIN_PATH}/roslin-project-status.sh ${ROSLIN_CORE_BIN_PATH}/bjp
 
-cat << "EOF"
-
- ______     ______     ______     __         __     __   __
-/\  == \   /\  __ \   /\  ___\   /\ \       /\ \   /\ "-.\ \
-\ \  __<   \ \ \/\ \  \ \___  \  \ \ \____  \ \ \  \ \ \-.  \
- \ \_\ \_\  \ \_____\  \/\_____\  \ \_____\  \ \_\  \ \_\\"\_\
-  \/_/ /_/   \/_____/   \/_____/   \/_____/   \/_/   \/_/ \/_/
-
-Add the following two lines to your .profile or .bashrc:
-
-EOF
-
-echo "source ${ROSLIN_CORE_CONFIG_PATH}/settings.sh"
-echo "export PATH=\${ROSLIN_CORE_BIN_PATH}:\$PATH"
+echo "DONE."
