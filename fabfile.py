d3bfe9bd891a3866a11d9284c4cb58181caf97ec
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-09-23 06:25:19 +0300

    Roslin Core + prism to roslin

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,7 @@
 
 import os
 import yaml
+import subprocess
 from fabric.api import *
 
 
@@ -12,10 +13,28 @@ def get_config():
         return yaml.load(file_handle.read())
 
 
-def get_settings_sh_path(config):
-    "get path to settings.sh from yaml config obj"
+def get_pipeline_settings_path(config):
+    "get path to pipeline settings.sh from yaml config obj"
 
     return os.path.join(config["root"], config["binding"]["core"], "bin/setup/settings.sh")
+
+
+def read_core_settings():
+    "read the Roslin Core settings"
+
+    command = ['bash', '-c', 'source ./core/config/settings.sh && env']
+
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE)
+
+    source_env = {}
+
+    for line in proc.stdout:
+        (key, _, value) = line.partition("=")
+        source_env[key] = value.rstrip()
+
+    proc.communicate()
+
+    return source_env
 
 
 @task
@@ -41,8 +60,8 @@ def half_delete_roslin_input():
 @task
 def rsync_luna(skip_install=False, skip_ref=False, local_bin_singularity=False):
     """
-    fab -i ~/.ssh/id_rsa -u chunj -H u36.cbio.mskcc.org rsync_luna
-    fab -i ~/.ssh/id_rsa -u chunj -H 127.0.0.1:7777 rsync_luna:skip_ref=True,local_bin_singularity=True
+    fab -i ~/.ssh/mskcc_id_rsa -u chunj -H u36.cbio.mskcc.org rsync_luna
+    fab -i ~/.ssh/mskcc_id_rsa -u chunj -H 127.0.0.1:7777 rsync_luna:skip_ref=True,local_bin_singularity=True
     """
 
     # get configuration from config.yaml
@@ -80,18 +99,39 @@ def rsync_luna(skip_install=False, skip_ref=False, local_bin_singularity=False):
 
     # use /usr/local/bin/singularity instead if necessary
     if local_bin_singularity:
-        settings_sh_path = get_settings_sh_path(config)
+        settings_sh_path = get_pipeline_settings_path(config)
         run('sed -i "s#/usr/bin/singularity#/usr/local/bin/singularity#g" {}'.format(settings_sh_path))
 
 
 @task
-def rsync_core():
+def install_core():
     """
-    fab -i ~/.ssh/id_rsa -u chunj -H selene.mskcc.org rsync_core
+    fab -i ~/.ssh/mskcc_id_rsa -u chunj -H selene.mskcc.org install_core
     """
 
-    with cd("../core"):
+    core_settings = read_core_settings()
+
+    core_version = core_settings["ROSLIN_CORE_VERSION"]
+    work_dir = '/scratch/roslin-{}/install'.format(env.user)
+
+    run('mkdir -m 775 -p {}'.format(work_dir))
+    # run('chmod g+w {}'.format(work_dir))
+
+    with lcd("./core"):
 
         local("./make-deployable-pkg.sh")
 
-        put('roslin-core-v1.0.0.tgz', '/scratch/chunj/')
+        pkg_tgz = 'roslin-core-v{}.tgz'.format(core_version)
+
+        put(pkg_tgz, work_dir, mode=0775)
+
+        with cd(work_dir):
+
+            run('rm -rf core-{}'.format(core_version))
+            run('mkdir -m 775 -p core-{}'.format(core_version))
+
+            run('tar xvzf {} -C core-{}'.format(pkg_tgz, core_version))
+
+            with cd("core-{}/bin/install".format(core_version)):
+
+                run('./install-core.sh')
