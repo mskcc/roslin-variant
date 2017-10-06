57215fe31d92360156f6a033216f8a7ba0e7faaa
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-10-07 00:19:00 +0300

    Minor update

--- compress.py
+++ compress.py
@@ -19,7 +19,6 @@ def compress(output_filename):
         "tar",
         "--exclude", ".DS_Store",
         "--exclude", "./setup/config/*.template.sh",
-        "--exclude", "./setup/data/assemblies",
 #--> fixme
         "--exclude", "./setup/scripts",
         "--exclude", "./setup/bin",
@@ -45,9 +44,9 @@ def compress(output_filename):
         # "--exclude", "./setup/img/vardict",
         # "--exclude", "./setup/img/vcf2maf",
         # "--exclude", "./setup/img/vep",
-
         # "--exclude", "./setup/img/bwa",
         # "--exclude", "./setup/img/samtools",
+
         # "--exclude", "./setup/examples",
 #<--
         "-cvzf", output_filename,
