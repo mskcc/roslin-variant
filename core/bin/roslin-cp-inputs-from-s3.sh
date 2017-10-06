6767f0226c8fa3ff1fb0ca2309b8991d6a261325
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-10-07 00:04:22 +0300

    Add script to copy inputs from/to s3

---
+++ core/bin/roslin-cp-inputs-from-s3.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+usage()
+{
+cat << EOF
+
+USAGE: `basename $0` [options]
+
+OPTIONS:
+
+   -u	   user id
+   -p      project name
+   -h      Help
+
+EOF
+}
+
+# defaults
+force_overwrite=0
+
+while getopts "u:p:h" OPTION
+do
+    case $OPTION in
+        u) user_id=$OPTARG ;;
+        p) proj_name=$OPTARG ;;
+        h) usage; exit 0 ;;
+        *) usage; exit 1 ;;
+    esac
+done
+
+if [ -z "$user_id" ] || [ -z "$proj_name" ]
+then
+    usage
+    exit 1
+fi
+
+s3_bucket="s3://roslin-installer-dev/workspace/${user_id}/${proj_name}"
+
+mkdir -p ${proj_name}
+aws s3 sync ${s3_bucket} ${proj_name}
+
+chmod +x ${proj_name}/run-example.sh
+
+echo "Make sure to update run-example.sh in ./${proj_name}/"
+echo "- Ensure Roslin Pipeline name and version is the one that you wish to use."
+echo "- roslin_submit.py is not supported."
+echo "- LSF is not supported."
+echo
