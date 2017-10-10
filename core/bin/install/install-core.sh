d4b7e91922d28a2412768ca25d7d7734d532f0e8
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-10-10 19:43:57 +0300

    Give write permission

--- core/bin/install/install-core.sh
+++ core/bin/install/install-core.sh
@@ -14,4 +14,7 @@ cp -r ../../config/* ${ROSLIN_CORE_CONFIG_PATH}
 cp -r ../../schemas/* ${ROSLIN_CORE_SCHEMA_PATH}
 cp ${ROSLIN_CORE_BIN_PATH}/roslin-project-status.sh ${ROSLIN_CORE_BIN_PATH}/bjp
 
+# give write permission
+chmod -R g+w ${ROSLIN_CORE_BIN_PATH}/install
+
 echo "DONE."
