2b5f37f35c5a0351b91d496ef683b9bcd117d5d4
Nikhil Kumar <nikhilkumar516@gmail.com>
2018-01-24 16:17:53 -0500

    Update install-packages.R

--- build/containers/roslin-qc/0.5.5/install-packages.R
+++ build/containers/roslin-qc/0.5.5/install-packages.R
@@ -1,6 +1,6 @@
 #!/usr/bin/Rscript
 
 # install required R libraries
-pkgs = c("ggplot2","gplots","corrplot","scales","reshape","plyr","RColorBrewer")
-install.packages("corrplot", repos='http://cran.us.r-project.org')
+pkgs = c("ggplot2","gplots","scales","reshape","plyr","RColorBrewer")
+install.packages("corrplot", repos='http://cran.wustl.edu')
 install.packages(pkgs, repos='http://cran.us.r-project.org')
