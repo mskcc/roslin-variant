715489068f14e06289b923eef38b88e6054e8fa8
Nikhil Kumar <nikhilkumar516@gmail.com>
2018-02-02 14:11:51 -0500

    Update settings-container.template.sh

--- build/scripts/settings-container.template.sh
+++ build/scripts/settings-container.template.sh
@@ -2,4 +2,6 @@
 
 # define bind points that need to be created inside containers
 # use a space character as a delimiter if you need to define multiple bind points
+export TMPDIR="/scratch"
+export TMP="/scratch"
 export SINGULARITY_BIND_POINTS="{{ binding_points}}"
