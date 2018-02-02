f19f95bad2ae148649b2eaa5da56cc1fe3beeadb
Nikhil Kumar <nikhilkumar516@gmail.com>
2018-02-02 15:42:44 -0500

    Update settings-container.template.sh

--- build/scripts/settings-container.template.sh
+++ build/scripts/settings-container.template.sh
@@ -4,4 +4,6 @@
 # use a space character as a delimiter if you need to define multiple bind points
 export TMPDIR="/scratch"
 export TMP="/scratch"
+echo 'export TMPDIR="/scratch"' >>$SINGULARITY_ENVIRONMENT
+echo 'export TMP="/scratch"' >>$SINGULARITY_ENVIRONMENT
 export SINGULARITY_BIND_POINTS="{{ binding_points}}"
