0bef70bb164df778a32b0718dbc30aa27354e11f
Allan Bolipata <bolipatc@mskcc.org>
2018-02-14 14:47:47 -0500

    Adding sv-only logging files to build.sh

--- test/build.sh
+++ test/build.sh
@@ -99,6 +99,14 @@ function store_test_logs {
     cp stdout.log $parentDir/$TestDir/test_stdout.txt
 }
 
+function store_test_logs_sv {
+    cd $installDir/roslin-pipelines/$ROSLIN_PIPELINE_NAME/$ROSLIN_PIPELINE_VERSION/outputs
+    cd $(ls -d */ | tail -n 1)
+    cd $(ls -d */ | head -n 1)
+    cp stderr.log $parentDir/$TestDir/test_stderr_sv.txt
+    cp stdout.log $parentDir/$TestDir/test_stdout_sv.txt
+}
+
 pipelineLeaderId=$(./run-example.sh | egrep -o -m 1 '[0-9]{8}')
 pipelineLeaderIdSV=$(./run-example-sv.sh | egrep -o -m 1 '[0-9]{8}')
 printf "project-workflow.cwl pipelineLeaderId: $pipelineLeaderId\nproject-workflow-sv.cwl pipelineLeaderIdSV: $pipelineLeaderIdSV\n"
@@ -117,6 +125,7 @@ do
     then
         printf "Both Jobs Finished Successfully\n"
         store_test_logs
+        store_test_logs_sv
         runningBool=0
     fi
 
@@ -140,32 +149,35 @@ do
         if [ "$leaderStatusSV" == "DONE" ] 
         then
             printf "Job SV Finished Successfully\n"
-            store_test_logs
+            store_test_logs_sv
             jobTrackBoolSV=0
         elif [ "$leaderStatusSV" == "EXIT" ]
         then
             printf "Job SV Failed\n"
-            store_test_logs
+            store_test_logs_sv
             jobTrackBoolSV=0
         fi
     fi
 
-    if [ $jobTrackBool == 0 ] && [ $jobTrackBoolSV == 0 ] && [ $runningBool != 0 ]
+    if [ $jobTrackBool == 0 ] && [ $jobTrackBoolSV == 0 ] 
     then
         if [ "$leaderStatus" == "EXIT" ] && [ "$leaderStatusSV" == "EXIT" ]
         then
             printf "Both Jobs Failed\n"
             store_test_logs
+            store_test_logs_sv
             exit 1    
         elif [ "$leaderStatus" == "EXIT" ] && [ "$leaderStatusSV" == "DONE" ]
         then
             printf "Regular Workflow Failed; SV Workflow Finished Successfully\n"
             store_test_logs
+            store_test_logs_sv
             exit 1
         elif [ "$leaderStatus" == "DONE" ] && [ "$leaderStatusSV" == "EXIT" ]
         then
             printf "Regular Workflow Finished Successfully; SV Workflow Failed\n"
             store_test_logs
+            store_test_logs_sv
             exit 1
         fi
     fi
