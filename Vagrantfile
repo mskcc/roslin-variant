e2996005f0b038fc457fa796a027333a39f2628e
Cyriac Kandoth <ckandoth@gmail.com>
2018-07-05 16:42:43 -0400

    Restore install-cmo; we will remove gxargparse later

--- Vagrantfile
+++ Vagrantfile
@@ -22,4 +22,5 @@ Vagrant.configure("2") do |config|
   config.vm.provision "shell", path: "./vm/install-singularity.sh"
   config.vm.provision "shell", path: "./vm/install-docker.sh"
   config.vm.provision "shell", path: "./vm/install-docker-registry.sh"
+  config.vm.provision "shell", path: "./vm/install-cmo.sh", privileged: false
 end
---
+++ vm/install-cmo.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# required for pysam/htslib introduced in 1.7.0
+# https://github.com/mskcc/cmo/commit/7eaefac72f45606ac1cfa8caeb1a0a47fef9695d
+sudo apt-get install -y build-essential zlib1g-dev libbz2-dev liblzma-dev libcurl4-gnutls-dev
+
+# required to do cwl postprocess
+sudo pip install pyyaml ruamel.yaml
+
+DEST_PATH="/usr/local/bin/"
+
+#---------------------------------
+# install cmo
+
+# get cmo from git
+sudo git clone https://github.com/mskcc/cmo.git ${DEST_PATH}/cmo-gxargparse/cmo
+sudo chown -R vagrant:vagrant ${DEST_PATH}/cmo-gxargparse/cmo
+
+# install dependencies
+#sudo pip install --upgrade pip
+
+# install
+cd ${DEST_PATH}/cmo-gxargparse/cmo
+python setup.py develop --user
+
+#---------------------------------
+# install gxargparse
+
+# get gxargparse from git
+sudo git clone https://github.com/common-workflow-language/gxargparse.git ${DEST_PATH}/cmo-gxargparse/gxargparse
+sudo chown -R vagrant:vagrant ${DEST_PATH}/cmo-gxargparse/gxargparse
+
+# install dependencies
+sudo pip install future
+sudo apt-get install -y python-lxml
+
+# install
+cd ${DEST_PATH}/cmo-gxargparse/gxargparse
+python setup.py install --user
