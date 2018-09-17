ffea4c1ee566522267d35364ca443fcab6957d5b
Allan Bolipata <bolipatc@mskcc.org>
2018-09-17 15:42:17 -0400

    Adding toil entry in configure.py

--- configure.py
+++ configure.py
@@ -56,6 +56,10 @@ def configure_setup_settings(settings):
             settings["dependencies"]["cmo"]["install-path"],
             settings["dependencies"]["cmo"]["version"],
             "lib/python2.7/site-packages"
+        ),
+        dependencies_toil_version=settings["dependencies"]["toil"]["version"],
+        dependencies_toil_install_path=os.path.join(
+            settings["dependencies"]["toil"]["install-path"]
         )
     )
 
