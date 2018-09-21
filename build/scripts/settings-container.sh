92bc26180d371f1a109035126c5dc69b2114f411
Nikhil Kumar <nikhilkumar516@gmail.com>
2018-09-21 12:56:22 -0400

    Removed settings artifact

--- build/scripts/settings-container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# define bind points that need to be created inside containers
-# use a space character as a delimiter if you need to define multiple bind points
-export TMPDIR="/scratch"
-export TMP="/scratch"
-echo 'export TMPDIR="/scratch"' >>$SINGULARITY_ENVIRONMENT
-echo 'export TMP="/scratch"' >>$SINGULARITY_ENVIRONMENT
-export SINGULARITY_BIND_POINTS="/ifs/work/pi/roslin-pipelines/bin /ifs/work/pi/roslin-pipelines/resources /ifs/work/pi/roslin-pipelines/outputs /ifs/work/pi/roslin-pipelines/workspace /scratch /ifs"
