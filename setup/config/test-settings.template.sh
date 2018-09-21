d338ebf63c3400c48fbfc3e61bc559ea6a48afb3
Nikhil Kumar <nikhilkumar516@gmail.com>
2018-09-21 12:46:33 -0400

    Changed configuration file templates

--- setup/config/test-settings.template.sh
+++ setup/config/test-settings.template.sh
@@ -1,5 +1,4 @@
 export ROSLIN_TEST_ROOT="{{ test_root }}"
 export TMPDIR="{{ test_tmp }}"
 export TMP="{{ test_tmp }}"
-export RUN_MODE="{{ test_run }}"
 {{ test_env }}
--- test/run-example-sv.template.sh
+++ test/run-example-sv.template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pipeline_name_version="PIPELINE_NAME/PIPELINE_VERSION"
+pipeline_name_version="{{ pipeline_name }}/{{ pipeline_version }}"
 
 roslin_request_to_yaml.py \
     --pipeline ${pipeline_name_version} \
@@ -9,11 +9,12 @@ roslin_request_to_yaml.py \
     -g Proj_DEV_0002_sample_grouping.txt \
     -r Proj_DEV_0002_request.txt \
     -o . \
-    -f inputs.yaml
+    -f inputs.yaml \
+    {{ run_args }}
 
 roslin_submit.py \
     --id Proj_DEV_0002 \
     --path . \
-    --workflow project-workflow.cwl \
+    --workflow project-workflow-sv.cwl \
     --single-node \
     --pipeline ${pipeline_name_version}
