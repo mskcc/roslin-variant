2c342534f55f92ae5b9da7e2d871a544519b36be
Allan Bolipata <bolipatc@mskcc.org>
2018-09-25 16:48:32 -0400

    Adding configure.py and config.variant.yaml... CONFIRMED TO WORK

--- configure.py
+++ configure.py
@@ -83,14 +83,10 @@ def configure_test_settings(settings):
     write_to_disk("setup/config/test-settings.sh", content)
 
     # Configure our example runs
-    run_example_template = get_template("test/run-example.template.sh")
-    run_example_content = run_example_template.render(pipeline_name=settings["name"],
+    run_pipeline_template = get_template("test/run-pipeline.template.sh")
+    run_pipeline_content = run_pipeline_template.render(pipeline_name=settings["name"],
         pipeline_version=settings["version"],run_args=settings["test"]["runArgs"])
-    write_to_disk("test/run-example.sh",run_example_content)
-    run_example_sv_template = get_template("test/run-example-sv.template.sh")
-    run_example_sv_content = run_example_sv_template.render(pipeline_name=settings["name"],
-        pipeline_version=settings["version"],run_args=settings["test"]["runArgs"])
-    write_to_disk("test/run-example-sv.sh",run_example_sv_content)
+    write_to_disk("test/run-pipeline.sh",run_pipeline_content)
 
 def configure_build_settings(settings):
     template = get_template("setup/config/build-settings.template.sh")
