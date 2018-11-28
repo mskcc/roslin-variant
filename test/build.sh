60fd6ad70aac9d15407045c1d29b772faa9ed37f
Allan Bolipata <bolipatc@mskcc.org>
2018-11-28 11:40:23 -0500

    Removed comments in build.sh

--- test/build.sh
+++ test/build.sh
@@ -12,7 +12,6 @@ printf "\n----------Running Test----------\n"
 cd $ROSLIN_PIPELINE_ROOT/workspace/jenkins/examples/Proj_DEV_0003
 mv $parentDir/test/run-example.sh .
 mv $parentDir/test/run-example-sv.sh .
-mv $parentDir/test/run-pipeline.sh .
 
 source $parentDir/setup/config/test-settings.sh
 rm -f $parentDir/setup/config/test-settings.sh
@@ -37,53 +36,36 @@ function store_test_logs_sv {
     cp stdout.log $parentDir/$TestDir/test_stdout_sv.txt
 }
 
-function store_test_logs_run_pipeline {
-    cd $ROSLIN_PIPELINE_ROOT/outputs 
-    cd $(ls -d */ | tail -n 1)
-    cd $(ls -d */ | head -n 1)
-    printf "Storing run_pipeline.py logs..."
-#    cp stderr.log $parentDir/$TestDir/test_stderr_run_pipeline.txt
-    cp stdout.log $parentDir/$TestDir/test_stdout_run_pipeline.txt
-}
-
 chmod +x run-example.sh
 chmod +x run-example-sv.sh
-chmod +x run-pipeline.sh
 
 pipelineLeaderId=$(./run-example.sh | egrep -o -m 1 '[0-9]{8}')
 pipelineLeaderIdSV=$(./run-example-sv.sh | egrep -o -m 1 '[0-9]{8}')
-#pipelineLeaderIdRP=$(./run-pipeline.sh | egrep -o -m 1 '[0-9]{8}')
-printf "project-workflow.cwl pipelineLeaderId: $pipelineLeaderId\nproject-workflow-sv.cwl pipelineLeaderIdSV: $pipelineLeaderIdSV\n" #Pipeline 2.5.0 ID: $pipelineLeaderIdRP\n"
+
+printf "project-workflow.cwl pipelineLeaderId: $pipelineLeaderId\nproject-workflow-sv.cwl pipelineLeaderIdSV: $pipelineLeaderIdSV\n" 
 runningBool=1
 
 while [ $runningBool != 0 ]
 do
     leaderStatus=$(bjobs $pipelineLeaderId | awk '{print $3}' | tail -1)
     leaderStatusSV=$(bjobs $pipelineLeaderIdSV | awk '{print $3}' | tail -1)
-#    leaderStatusRP=$(bjobs $pipelineLeaderIdRP | awk '{print $3}' | tail -1)
 
-    printf "Regular: $leaderStatus; SV: $leaderStatusSV\n" #; RP: $leaderStatusRP\n"
+    printf "Regular: $leaderStatus; SV: $leaderStatusSV\n"
 
-    if [ "$leaderStatus" == "DONE" ] && [ "$leaderStatusSV" == "DONE" ] #&& [ "$leaderStatusRP" == "DONE" ]
+    if [ "$leaderStatus" == "DONE" ] && [ "$leaderStatusSV" == "DONE" ] 
     then
         printf "All Jobs Finished Successfully\n"
         store_test_logs
         store_test_logs_sv
-#        store_test_logs_run_pipeline
         runningBool=0
     fi
 
-    if [ "$leaderStatus" == "EXIT" ] || [ "$leaderStatusSV" == "EXIT" ] # || [ "$leaderStatusRP" == "EXIT" ]
+    if [ "$leaderStatus" == "EXIT" ] || [ "$leaderStatusSV" == "EXIT" ] 
     then
         printf "One or more of the jobs have failed\n"
-#        if [ "$leaderStatusRP" != "EXIT" ]
-#        then
-#            `bkill $leaderStatusRP`
-#        fi
         runningBool=0
         store_test_logs
-        store_test_logs_sv
-#        store_test_logs_run_pipeline
+        store_test_logs_sv#
         exit 1
     fi
     sleep 1m
