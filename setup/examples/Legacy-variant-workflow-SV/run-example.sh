9412277a4d07f9aa29a8668875c8ab7eda1bdd10
Nikhil <nikhilkumar516@gmail.com>
2019-01-24 16:39:28 -0500

    Added tests for each step in each workflow

---
+++ setup/examples/Legacy-variant-workflow-SV/run-example.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+pipeline_name=${ROSLIN_PIPELINE_NAME}
+pipeline_version=${ROSLIN_PIPELINE_VERSION}
+
+roslin_request_to_yaml.py \
+    --pipeline ${pipeline_name}/${pipeline_version} \
+    -m Proj_DEV_0003_sample_mapping.txt \
+    -p Proj_DEV_0003_sample_pairing.txt \
+    -g Proj_DEV_0003_sample_grouping.txt \
+    -r Proj_DEV_0003_request.txt \
+    -o . \
+    -f inputs.yaml
+
+roslin_submit.py \
+    --name ${pipeline_name} \
+    --version ${pipeline_version} \
+    --id Proj_DEV_0003 \
+    --inputs inputs.yaml \
+    --path . \
+    --workflow LegacyVariantWorkflowSV \
+    --batch-system singleMachine \
+    --foreground-mode
