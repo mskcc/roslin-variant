e4f473f73f7b868c7e8b04bccc98e5acda94a284
Nikhil <nikhilkumar516@gmail.com>
2019-01-30 14:18:03 -0500

    Some cleanup

--- configure.py
+++ configure.py
@@ -100,7 +100,7 @@ def configure_test_settings(settings):
     template = get_template("setup/config/test-settings.template.sh")
     test_env_str = ""
     for single_env_key, single_env_val in settings["test"]["env"].items():
-        test_env_str = test_env_str + "export " + single_env_key + '="' + single_env_val + '"\n' 
+        test_env_str = test_env_str + "export " + single_env_key + '="' + single_env_val + '"\n'
 
     content = template.render( test_root=settings["test"]["root"],
         test_tmp=settings["test"]["tempDir"],
@@ -108,20 +108,6 @@ def configure_test_settings(settings):
     )
 
     write_to_disk("setup/config/test-settings.sh", content)
-
-    # Configure our example runs
-    run_example_template = get_template("test/run-example.template.sh")
-    run_example_content = run_example_template.render(pipeline_name=settings["name"],
-        pipeline_version=settings["version"],run_args=settings["test"]["runArgs"])
-    write_to_disk("test/run-example.sh",run_example_content)
-    run_example_sv_template = get_template("test/run-example-sv.template.sh")
-    run_example_sv_content = run_example_sv_template.render(pipeline_name=settings["name"],
-        pipeline_version=settings["version"],run_args=settings["test"]["runArgs"])
-    write_to_disk("test/run-example-sv.sh",run_example_sv_content)
-    run_pipeline_template = get_template("test/run-pipeline.template.sh")
-    run_pipeline_content = run_pipeline_template.render(pipeline_name=settings["name"],
-        pipeline_version=settings["version"],run_args=settings["test"]["runArgs"])
-    write_to_disk("test/run-pipeline.sh",run_pipeline_content)
 
 def configure_build_settings(settings):
     template = get_template("setup/config/build-settings.template.sh")
