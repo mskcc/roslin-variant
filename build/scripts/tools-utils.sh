8d8d0403afbeea0af7762ea9c9b268bc43dad42e
Nikhil <nikhilkumar516@gmail.com>
2019-02-28 15:42:11 -0500

    Some cleanup

--- build/scripts/tools-utils.sh
+++ build/scripts/tools-utils.sh
@@ -11,11 +11,6 @@ function get_tool_version {
     echo `echo $1 | awk -F':' '{ print $2 }'`
 }
 
-# input example -> bwa:0.7.12:cmo_bwa_mem
-function get_cmo_wrapper_name {
-    echo `echo $1 | awk -F':' '{ print $3 }'`
-}
-
 function is_tool_available {
     tool_name=$(get_tool_name $1)
     tool_version=$(get_tool_version $1)
@@ -31,10 +26,4 @@ function is_tool_available {
 # e.g. samtools:1.3.1,trimgalore:0.4.3
 function get_tools_name_version {
     echo `python tools_utils.py get_name_version`
-}
-
-# this returns comma-separated list of all tools instalalble including correspodning cwl wrapper
-# e.g. bwa:0.7.12:cmo_bwa_mem,trimgalore:0.4.3:cmo_trimgalore
-function get_tools_name_version_cmo {
-    echo `python tools_utils.py get_name_version_cmo`
 }
