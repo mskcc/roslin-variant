e907464833f091f9b352440e4767b7c677ad16d2
Nikhil <nikhilkumar516@gmail.com>
2019-02-28 15:57:41 -0500

    Slight change in copying build files

---
+++ build/scripts/move-artifacts-to-setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# load utils
+source ./tools-utils.sh
+source ./settings-build.sh
+
+rm -rf ${IMG_DIRECTORY}
+mkdir -p ${IMG_DIRECTORY}
+
+# copy over all singularity images whose name/version matches with what's defined in tools.json
+all_avail_tools=$(get_tools_name_version)
+for tool_info in $(echo $all_avail_tools | sed "s/,/ /g")
+do
+    tool_name=$(get_tool_name $tool_info)
+    tool_version=$(get_tool_version $tool_info)
+    tool_output_dir="${IMG_DIRECTORY}/${tool_name}/${tool_version}"
+
+    # don't copy if tool name starts with @
+    if [ ${tool_name:0:1} == "@" ]
+    then
+        continue
+    fi
+
+    mkdir -p ${tool_output_dir}
+
+    cp ${CONTAINER_DIRECTORY}/${tool_name}/${tool_version}/${tool_name}.sif ${tool_output_dir}
+
+done
+
+cp ${CONTAINER_DIRECTORY}/images_meta.json ${IMG_DIRECTORY}
+
+echo "--- IMAGE TREE ---"
+# show tree
+tree ${IMG_DIRECTORY}
+
+# get md5 checksum for all image files
+cd ${IMG_DIRECTORY}
+find . -name "*.sif" -type f | xargs md5sum > ${IMG_DIRECTORY}/checksum.dat
+
+# get md5 checksum for all cwl files
+echo "--- CWL TREE ---"
+
+cd ${CWL_DIRECTORY}
+find . -name "*.cwl" -type f | xargs md5sum > ${CWL_DIRECTORY}/checksum.dat
