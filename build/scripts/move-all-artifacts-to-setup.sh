8ab5ca4e6ab0368262676a61f2f8c8c738c10cee
Nikhil <nikhilkumar516@gmail.com>
2019-02-28 15:56:09 -0500

    Some cleanup

--- build/scripts/move-all-artifacts-to-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-######################################
-## RUN THIS FROM INSIDE VAGRANT BOX
-######################################
-
-./move-container-artifacts-to-setup.sh
-#./move-cwl-artifacts-to-setup.sh
