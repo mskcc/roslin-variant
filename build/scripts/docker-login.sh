f8c86c9e27878516a8a68605b6cb3e9189b00518
Nikhil <nikhilkumar516@gmail.com>
2019-02-28 15:59:39 -0500

    Added standalone script for docker-login

---
+++ build/scripts/docker-login.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# check if user is logged in to Docker Hub.
+# the following command returns auth token if user is authenticated
+if sudo test -f ~/.docker/config.json
+then
+    is_login=`sudo cat ~/.docker/config.json |  python -c 'import sys, json; print len(json.load(sys.stdin)["auths"])'`
+else
+    is_login="0"
+fi
+
+if [ "$is_login" == "0" ]
+then
+    sudo docker login
+    if [ $? -ne 0 ]
+    then
+        echo "You must be logged in to Docker Hub. Please try again."
+        exit 1
+    fi
+fi
