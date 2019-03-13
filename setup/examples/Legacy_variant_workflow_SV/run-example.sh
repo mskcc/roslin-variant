bf3a2f74cab3fdffbd7db3133807ce5bc0e08984
nikhil <nikhilkumar516@gmail.com>
2019-03-13 00:52:29 -0400

    Updated test examples

--- setup/examples/Legacy_variant_workflow_SV/run-example.sh
+++ setup/examples/Legacy_variant_workflow_SV/run-example.sh
@@ -5,6 +5,7 @@ pipeline_version=${ROSLIN_PIPELINE_VERSION}
 
 roslin_request_to_yaml.py \
     --pipeline ${pipeline_name}/${pipeline_version} \
+    --clinical Legacy_variant_workflow_SV_sample_data_clinical.txt \
     -m Legacy_variant_workflow_SV_sample_mapping.txt \
     -p Legacy_variant_workflow_SV_sample_pairing.txt \
     -g Legacy_variant_workflow_SV_sample_grouping.txt \
