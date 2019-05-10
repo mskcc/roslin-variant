7cf98c695bee7fbe923bf0eefb512633149e8f59
nikhil <nikhilkumar516@gmail.com>
2019-05-10 11:22:55 -0400

    Updated runscript in java images

--- build/containers/gatk/3.3-0/runscript.sh
+++ build/containers/gatk/3.3-0/runscript.sh
@@ -1,8 +1,30 @@
 if [ "$1" = "help" ]
 then
-	exec java -jar /usr/bin/gatk.jar --help
+	exec java -jar  /usr/bin/gatk.jar
 fi
 
-java_args=$1
-shift
-exec java $java_args /usr/bin/gatk.jar $@
+java_opts=""
+tool_opts=""
+
+flag=0
+for var in $@
+do
+
+    if [ $flag -eq 0 ]
+    then
+        # we're handling java options
+        java_opts="$java_opts $var"
+    else
+        # we're handling tool options
+        tool_opts="$tool_opts $var"
+    fi
+
+    if [ "$var" = "-jar" ]
+    then
+        # we're done with handling java options
+        flag=1
+    fi
+
+done
+
+exec java $java_opts /usr/bin/gatk.jar $tool_opts
--- build/containers/picard/2.9/runscript.sh
+++ build/containers/picard/2.9/runscript.sh
@@ -3,6 +3,28 @@ then
 	exec java -jar /usr/bin/picard-tools/picard.jar
 fi
 
-java_args=$1
-shift
-exec java $java_args /usr/bin/picard-tools/picard.jar $@
+java_opts=""
+tool_opts=""
+
+flag=0
+for var in $@
+do
+
+    if [ $flag -eq 0 ]
+    then
+        # we're handling java options
+        java_opts="$java_opts $var"
+    else
+        # we're handling tool options
+        tool_opts="$tool_opts $var"
+    fi
+
+    if [ "$var" = "-jar" ]
+    then
+        # we're done with handling java options
+        flag=1
+    fi
+
+done
+
+exec java $java_opts /usr/bin/picard-tools/picard.jar $tool_opts
