9c5bc3dfdf15dbef57c52400681b1687b7de180c
nikhil <nikhilkumar516@gmail.com>
2019-06-07 16:00:50 -0400

    Updated nvm link for better installation of node

--- build/scripts/build-node.sh
+++ build/scripts/build-node.sh
@@ -5,7 +5,7 @@ export HOME=$ROSLIN_PIPELINE_RESOURCE_PATH
 export NVM_DIR="$ROSLIN_PIPELINE_RESOURCE_PATH/.nvm"
 # Setup node
 mkdir $ROSLIN_PIPELINE_RESOURCE_PATH/.nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 source $ROSLIN_PIPELINE_RESOURCE_PATH/.nvm/nvm.sh
 nvm install node
 export HOME=$HOME_TEMP
