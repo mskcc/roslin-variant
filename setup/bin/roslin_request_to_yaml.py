56c05bf3819c33952c622e52c4abb5b99bee1efb
Allan Bolipata <bolipatc@mskcc.org>
2019-07-26 10:51:27 -0400

    Add support for 1 new IMPACT and 3 new WES assays

--- setup/bin/roslin_request_to_yaml.py
+++ setup/bin/roslin_request_to_yaml.py
@@ -105,10 +105,11 @@ def parse_request_file(rfile):
 
 
 def get_curated_bams(assay,REQUEST_FILES):
-    # Default to IMPACT468 BAMs for all projects except Agilent/IDTdna Exomes
-    json_curated_bams = REQUEST_FILES['curated_bams']['IMPACT468_b37']
-    if assay.find("AgilentExon_51MB_b37_v3") > -1 or assay.find("Agilent_v4_51MB_Human") > -1:
-        json_curated_bams = REQUEST_FILES['curated_bams']['AgilentExon_51MB_b37_v3']
+    # Default to AgilentExon_51MB_b37_v3 BAMs for all assays except those specified below
+    json_curated_bams = REQUEST_FILES['curated_bams']['AgilentExon_51MB_b37_v3']
+    # Default to IMPACT468_b37 BAMs for all IMPACT/HemePACT assays
+    if assay.find("IMPACT") > -1 or assay.find("HemePACT") > -1:
+        json_curated_bams = REQUEST_FILES['curated_bams']['IMPACT468_b37']
     elif assay.find("IDT_Exome_v1_FP") > -1:
         json_curated_bams = REQUEST_FILES['curated_bams']['IDT_Exome_v1_FP_b37']
     array = []
