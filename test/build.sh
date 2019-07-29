c3f772bf4418a90a9006a8739c1f0a1787e6bf7e
Nikhil Kumar <nikhilkumar516@gmail.com>
2019-07-29 17:06:49 -0400

    Some cleanup

--- test/build.sh
+++ test/build.sh
@@ -2,16 +2,12 @@
 # Set estimated walltime <60 mins and use the barely used internet nodes, to reduce job PEND times
 cd ..
 source build-pipeline.sh -t -b $BUILD_NUMBER
-
 # Run test
 printf "\n----------Running Test----------\n"
-
 cd $ROSLIN_EXAMPLE_PATH
 source $parentDir/setup/config/test-settings.sh
 rm -f $parentDir/setup/config/test-settings.sh
-
 export TMPDIR_TEST=$TMPDIR
 export LOG_TEST=$1
-
 export PATH=$ROSLIN_CORE_BIN_PATH:$PATH
 pytest -n 8 $ROSLIN_CORE_BIN_PATH/roslin_workflows_test.py
